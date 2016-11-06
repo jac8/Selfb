d333f545255c0dc5d594442d9810d46859daf331
jac8 <mrjac9@gmail.com>
2016-11-06 10:35:46 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v2 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "Self-Bot v2 \n by Me :) <3 \n @HellSudo"
 end 
 return {
   patterns = {
