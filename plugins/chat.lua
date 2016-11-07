e6bf479f2ee37b902ba8b8fc17ec575bd9b58013
jac8 <mrjac9@gmail.com>
2016-11-07 10:35:39 +0330

    Create chat.lua

---
+++ plugins/chat.lua
@@ -0,0 +1,75 @@
+#Ðɪℓαη ♡ sαяα:
+local function run(msg)
+if msg.text == "hi" then
+  return "سلام👻"
+end
+if msg.text == "Hi" then
+  return "سلام👻"
+end
+if msg.text == "Hello" then
+  return "سلام👻"
+end
+if msg.text == "hello" then
+  return "سلام👻"
+end
+if msg.text == "Salam" then
+  return "علیک👻"
+end
+if msg.text == "salam" then
+  return "علیک سلام 😃"
+end
+if msg.text == "dilan" then
+  return "jun👻"
+end
+if msg.text == "Dilan" then
+  return "joon❤️"
+end
+if msg.text == "دیلی" then
+  return "بنال 😂"
+end
+if msg.text == "دیلان" then
+  return "❣جونم "
+end
+if msg.text == "سلام" then
+  return "سلام ♥️"
+end
+if msg.text == "slm" then
+  return "سلام👻"
+end
+if msg.text == "Slm" then
+  return "سلام 😃"
+end
+if msg.text == "بای" then
+  return "بای بای "
+end
+if msg.text == "خدافظ" then
+  return "Sick❌"
+end
+if msg.text == "bye" then
+  return "خدافظ"
+end
+if msg.text == "@Dilan" then
+  return "😃ها "
+end
+end
+
+return {
+  description = "Chat With Robot", 
+  usage = "chat with robot",
+  patterns = {
+    "^[Hh]i$",
+    "^[Hh]ello$",
+    "^سلام$",
+    "^[Dd]ilan$",
+    "^[Bb]ye$",
+    "^[Ss]alam$",
+    "^@Dilan$",
+    "^خدافظ$",
+    "^بای$",
+    "^[Ss]lm$",
+    "^دیلی$",
+    "^دیلان$",
+    }, 
+  run = run,
+  pre_process = pre_process
+}
